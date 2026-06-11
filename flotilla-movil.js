a9998c76c8694972183e9fc6374073870b054a7c
mercadotecniatecnocontrol-dotcom <mercadotecniatecnocontrol@gmail.com>
2026-06-11 12:45:01 -0600

    Add email 's.mendoza@tecnocontrol.com.mx' to list

--- flotilla-movil.js
+++ flotilla-movil.js
@@ -107,6 +107,7 @@ const ADMINS_FLOTILLA=[
   'tomas@tecnocontrol.com.mx',
   'fernando@tecnocontrol.com.mx',
   'v.garcia@tecnocontrol.com.mx',
+  's.mendoza@tecnocontrol.com.mx',
   'i.saucedo@tecnocontrol.com.mx',
   'plazajrz@tecnocontrol.com.mx',
   'i.ruiz@jomarverifica.com.mx',
