a7a93471df317a2f411afe891a466cdce546f344
mercadotecniatecnocontrol-dotcom <mercadotecniatecnocontrol@gmail.com>
2026-06-11 12:50:46 -0600

    Update flotilla-movil.js

--- flotilla-movil.js
+++ flotilla-movil.js
@@ -107,7 +107,6 @@ const ADMINS_FLOTILLA=[
   'tomas@tecnocontrol.com.mx',
   'fernando@tecnocontrol.com.mx',
   'v.garcia@tecnocontrol.com.mx',
-  's.mendoza@tecnocontrol.com.mx',
   'i.saucedo@tecnocontrol.com.mx',
   'plazajrz@tecnocontrol.com.mx',
   'i.ruiz@jomarverifica.com.mx',
